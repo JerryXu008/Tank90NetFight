e9209626eacc760e5bfde5819ec3aed4aa3002e8
JerryXu <573398127@qq.com>
2016-10-31 15:07:05 +0800

    add aaa.php

---
+++ Tank/aaa.php
@@ -0,0 +1,113 @@
+
+
+
+/Users/song/Documents/未命名文件夹
+
+
+
+/Users/song/Desktop/cocos2dStudy/cocos2d-x-3.13.1/tools/cocos2d-console/bin/HelloCCSuperMan
+
+
+
+
+
+
+
+cd /Users/song/Desktop/cocos2dStudy/cocos2d-x-3.13.1/tools/cocos2d-console/bin/HelloCC
+song-Mac-mini:HelloCC song$ node app.js
+
+
+CTTelephonyNetworkInfo
+
+_sqlite3_clear_binding
+
+
+
+https://api.weixin.qq.com/sns/oauth2/access_token?code=1314519664&secret=d7a748031352af5ce4d843a2b3b26ec5&appid=wx5fe53f16645c070a&grant_type=authorization_code
+
+
+
+ "access_token" = "-Dbw73nPLjNsI53Gx6zjX-phvOd53yIhoxJ-KYdVVVaES-7hUROwwBnDAtXaSzhTwH-7WZxcUDTtzmFn8dT7CCdIDbay92EVuDTDO_acNHI";
+    "expires_in" = 7200;
+    openid = o03FOwNyZuLEGyT3YtYWGVxykhNI;
+    "refresh_token" = "KxoBK9VgVWPAHvT52elXF_xOmnPoSVk1vi7xG6wSm6ilM4-es06m8oZU-z2JXJHVC_qV-X2MSP7rEe-eIj-tVMVeIreailMYeGz0Ys2X2Go";
+    scope = "snsapi_userinfo";
+    unionid = owpITwdkMi9BBWtFWwZVBz8mpUds;
+
+
+
+
+
+
+
+com.mob.demoShareSDK
+
+/Users/song/Desktop/cocos2dStudy/cocos2d-x-3.13.1/tools/cocos2d-console/bin/HelloCC
+
+  
+ /Users/song/Desktop/cocos2dStudy/cocos2d-x-3.13.1/tools/cocos2d-console/bin
+
+
+-(void)ReceiveAccountID:(NSString *)accountId returnType:(NSString *)type
+
+
+/Users/song/Downloads/PengYouQuan/PengYouQuan/种子切图/searchm@2x.png
+
+
+/Users/song/Downloads/归档/YiDongSheJiao/WebViewJavascriptBridge/UIWebView+JavaScriptAlert.m
+/Users/song/Downloads/PengYouQuan/PengYouQuan/WebViewJavascriptBridge/UIWebView+JavaScriptAlert.m
+/Users/song/Desktop/宝贝备份/备份/本人设计模块(新)/iphone开发系列/iphone开发经典项目点评与详解/多线程简单示例/iOS多线程编程指南.pdf
+
+/Users/song/Downloads/宝贝多移动社交/YiDongSheJiao/WebViewJavascriptBridge/UIWebView+JavaScriptAlert.m
+
+1 互动列表接口/Users/song/Desktop/宝贝备份/备份/完美webview.zip
+
+ 返回缺少 1 图片字段， 2 应该根据我传过去的x,y经纬度返回 距离字段
+
+2 活动详情接口 缺少 1 距离字段 2 企业简介（没找到对应字段） ，返回的数组，我这边解析不对
+
+3 个人奖励接口    身份证号是哪个字段
+
+          "activity_id" = 1;
+         closed = 0;
+         "end_time" = "2016-05-29 22:12:14";
+         "exchange_type" = 1;
+         "good_name" = sfd;
+         name = "\U53ef\U4e50\U4e00\U74f6";
+         "person_no" = 1;
+         phone = 12345679830;
+
+          
+     1 却少  公司名，图片地址，获奖时间，地址
+     2 缺少  奖励ID，点击兑付的时候需要
+     3 缺少 兑付奖励的接口
+       个人奖励点击兑付的时候，需要走 活动详情接口，活动详情接口现在返回的是数组，需要调整
+
+/Users/song/Desktop/宝贝备份/项目/河北电台/IOSXinDianTai/TestHebei/Images/f_btnback.png
+4.商家发布列表
+ 添加点数据看看
+
+5.获奖名单 返回 315101 查询错误
+  http://123.56.156.196/index.php/Ctemplate/Active/getWard
+{
+    "activity_id" = 1;
+    page = 1;
+}
+
+
+6，发布活动，需要传参数x坐标和y坐标
+
+7 商家资料修改失败
+
+http://123.56.156.196/index.php/Ctemplate/Active/company_info
+{
+    "company_address" = fsdfdsfdsfdsfdsfdsfdsfdsf;
+    "company_id" = 2;
+    "company_name" = "\U5b9d\U8d1d\U591a";
+    "company_phone" = 13287781387;
+    "company_photo" = "";
+    "company_readme" = fdsfdsfdsfdsfdsfsdfdsfsf;
+    "company_url" = "";
+}
+
+  
